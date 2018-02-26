111987bba25448417b9a94b899b945d07d46b5e0
Miguel Ferrer <ingferrermiguel@gmail.com>
2018-02-26 08:30:04 -0500

    Add MANIFEST.in; Bump ver. 0.1.2

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+import os
 from setuptools import setup
 
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 setup(name='instagram-python-lib',
-      version='0.1',
+      version='0.1.2',
       description='API wrapper for Instagram written in Python',
+      long_description=read('README.md'),
       url='https://github.com/GearPlug/instagram-python',
       author='Miguel Ferrer',
       author_email='ingferrermiguel@gmail.com',
